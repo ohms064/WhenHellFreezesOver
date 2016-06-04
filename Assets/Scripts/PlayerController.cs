edac4abfd0fc74d32c32a568376aa1cefe8c53c0
Omar Rodríguez Pérez <otokonoko064@gmail.com>
2016-06-03 23:07:16 -0500

    Archivos iniciales.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    private Animator _animator;
+    private float _movimiento;
+	// Use this for initialization
+	void Start () {
+	    _animator = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+    void Update() {
+        _movimiento = Input.GetAxis( "Horizontal" );
+        if ( _movimiento != 0 && !_animator.GetBool( "Running" ) ) { StartCoroutine( "Run" ); }
+    }
+
+    IEnumerator Run() {
+        _animator.SetBool( "Running", true );
+        
+        do {
+            this.transform.position += new Vector3( _movimiento*Time.deltaTime, 0.0f, 0.0f );
+            yield return new WaitForEndOfFrame();
+        }
+        while ( _movimiento != 0.0f );
+        _animator.SetBool( "Running", false );
+    }
+}
