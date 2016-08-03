eaf97c9025dca5bd6f51510b035da347967157e2
Omar Rodríguez Pérez <otokonoko064@gmail.com>
2016-08-02 21:20:55 -0500

    Ya se puede comprar más tiempo del efecto de congelar.

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,13 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class UIManager : MonoBehaviour {
     public Canvas mainMenu;
     public Canvas storeMenu;
+    public Text coinsText;
     public LoadScene loadScene;
+
+    private string defaultString;
 
     void Start() {
         loadScene.sceneToLoad = FileManager.instance.currentGameStatusJson.lastLevel;
+        storeMenu.enabled = false;
+        defaultString = coinsText.text;
+        UpdateCoins();
     }
 
     public void ShowMainMenu() {
@@ -18,6 +25,20 @@ public class UIManager : MonoBehaviour {
     public void ShowStoreMenu() {
         mainMenu.enabled = false;
         storeMenu.enabled = true;
+    }
+
+    public void BuyFreezeUpgrade() {
+        int coins = FileManager.instance.currentGameStatusJson.coins;
+        if ( coins <= 0 ) return;
+        FileManager.instance.currentFreezeJson.freezeTime += 5;
+        FileManager.instance.currentGameStatusJson.coins -= 2;
+        FileManager.instance.SaveAll();
+        UpdateCoins();
+    }
+
+    public void UpdateCoins() {
+        string txt = defaultString.Replace( "<number>", FileManager.instance.currentGameStatusJson.coins.ToString() );
+        coinsText.text = txt;
     }
 
 }
