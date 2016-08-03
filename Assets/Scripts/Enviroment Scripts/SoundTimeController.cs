b0585a25afcb279441ad2861ecd4415d7312eb59
Omar Rodríguez Pérez <otokonoko064@gmail.com>
2016-08-02 22:24:26 -0500

    Se agregaron efectos de audio a los prefabs de fuego.

---
+++ Assets/Scripts/Enviroment Scripts/SoundTimeController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class SoundTimeController : MonoBehaviour, IFreezable {
+    private AudioSource _source;
+
+    // Use this for initialization
+    void Start() {
+        _source = GetComponent<AudioSource>();
+    }
+
+    public void Freeze() {
+        _source.Stop();
+    }
+
+    public void FrozenRotation( float rotation ) {
+        return;
+    }
+
+    public void Unfreeze() {
+        _source.Play();
+    }
+
+}
