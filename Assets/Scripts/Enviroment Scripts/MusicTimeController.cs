cd31663424bc9fb6415de17739cd13ff523a2a64
Omar Rodríguez Pérez <otokonoko064@gmail.com>
2016-08-03 21:26:33 -0500

    Se creó el MusicTimeController.

---
+++ Assets/Scripts/Enviroment Scripts/MusicTimeController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MusicTimeController : MonoBehaviour, IFreezable {
+
+    private AudioSource _source;
+    public float freezePitch = 0.5f;
+    public float slowTime = 1.0f;
+
+    // Use this for initialization
+    void Start () {
+        _source = GetComponent<AudioSource>();
+        slowTime = 1 / slowTime;
+	}
+
+    public void Freeze() {
+        StopCoroutine( "Resume" );
+        StartCoroutine( "Slower" );
+    }
+
+    public void FrozenRotation( float rotation ) {
+        return;
+    }
+
+    public void Unfreeze() {
+        StopCoroutine( "Slower" );
+        StartCoroutine( "Resume" );
+    }
+
+    IEnumerator Slower() {
+        float pitch = _source.pitch;
+        float initTime = Time.time;
+        while(pitch > freezePitch ) {
+            _source.pitch = Mathf.Lerp( 1.0f, freezePitch, (Time.time - initTime) * slowTime );
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    IEnumerator Resume() {
+        float pitch = _source.pitch;
+        float initTime = Time.time;
+        while ( pitch < 1.0f ) {
+            _source.pitch = Mathf.Lerp( freezePitch, 1.0f, (Time.time - initTime) * slowTime );
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
